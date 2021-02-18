8dc4863fe59629dfaebc65401be3bcab94931ef6
Ahmet FURTUN <ahmet.furtun1@gmail.com>
2021-02-18 11:32:52 +0300

    First commit

---
+++ src/components/app/app.component.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import Calculator from '../calculator/calculator.component';
+import './app.component.css';
+
+const App:FC = () => {
+    return (
+        <div>
+            <Calculator/>
+        </div>
+    )
+}
+export default App;
---
+++ src/components/calculator/calculator.component.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './calculator.component.css';
+
+const Calculator: FC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Calculator;
