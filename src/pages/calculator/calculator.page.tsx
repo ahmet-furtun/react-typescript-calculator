58560f3ee0a2f4a7ccd24231d97fe5291b218806
Ahmet FURTUN <ahmet.furtun1@gmail.com>
2021-02-19 13:57:17 +0300

    Design finished

--- src/pages/calculator/calculator.page.tsx
+++ src/pages/calculator/calculator.page.tsx
@@ -7,30 +7,31 @@ const Calculator: FC = () => {
       <div className="calculator-container">
         <div className="result-screen"></div>
         <div className="buttons-screen">
-          <div className="item1">CE</div>
-          <div className="item2">DEL</div>
-          <div className="item2">/</div>
+          <div className="item1 CE">C</div>
+          <div className="item2 DEL">DEL</div>
+          <div className="item2 operator">/</div>
 
           <div className="item2 number">1</div>
           <div className="item2 number">2</div>
           <div className="item2 number">3</div>
-          <div className="item2">*</div>
+          <div className="item2 operator">*</div>
 
           <div className="item2 number">4</div>
           <div className="item2 number">5</div>
           <div className="item2 number">6</div>
-          <div className="item2">+</div>
+          <div className="item2 operator">+</div>
 
           <div className="item2 number">7</div>
           <div className="item2 number">8</div>
           <div className="item2 number">9</div>
-          <div className="item2">-</div>
+          <div className="item2 operator">-</div>
 
           <div className="item1 number">0</div>
           <div className="item2 number">.</div>
-          <div className="item2">=</div>
+          <div className="item2 operator">=</div>
         </div>
       </div>
+      <div className="footer">Simple Calculator</div>
     </div>
   );
 };
