ec59938671c3d37c16d3e3f4697021bcb6f268de
Ahmet FURTUN <ahmet.furtun1@gmail.com>
2021-02-19 13:19:14 +0300

    CSS design structer completely changed

--- src/pages/calculator/calculator.page.tsx
+++ src/pages/calculator/calculator.page.tsx
@@ -3,21 +3,32 @@ import "./calculator.page.css";
 
 const Calculator: FC = () => {
   return (
-    <div className="calculator-container">
-      <div className="calculator">
+    <div className="container">
+      <div className="calculator-container">
         <div className="result-screen"></div>
-        <div className="functionality-container">
-          <div className="erase-container"></div>
-          <div className="down-keys-container">
-            <div className="number-container"></div>
-            <div className="operator-container">
-                <span>+</span>
-                <span>-</span>
-                <span>/</span>
-                <span>x</span>
-                <span>=</span>
-            </div>
-          </div>
+        <div className="buttons-screen">
+          <div className="item1">CE</div>
+          <div className="item2">DEL</div>
+          <div className="item2">/</div>
+
+          <div className="item2 number">1</div>
+          <div className="item2 number">2</div>
+          <div className="item2 number">3</div>
+          <div className="item2">*</div>
+
+          <div className="item2 number">4</div>
+          <div className="item2 number">5</div>
+          <div className="item2 number">6</div>
+          <div className="item2">+</div>
+
+          <div className="item2 number">7</div>
+          <div className="item2 number">8</div>
+          <div className="item2 number">9</div>
+          <div className="item2">-</div>
+
+          <div className="item1 number">0</div>
+          <div className="item2 number">.</div>
+          <div className="item2">=</div>
         </div>
       </div>
     </div>
